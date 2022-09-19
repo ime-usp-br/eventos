8247b0106a74b2f54a8b77234e4b228bbb318698
Ayrton Douglas Freitas Guimarães <ayrtondouglas@usp.br>
2022-09-19 10:09:33 -0300

    fix: foram feitas correções na api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -10,50 +10,21 @@ class ApiController extends Controller
 {
     public function getAllEvents()
     {
-        $eventos = Event::with(["idioma","modalidade","tipo","anexos"])->where("aprovado", true)->get();
-        $eventos = $eventos->filter(function($evento){
-                if($evento->dataFinal){
-                    if($evento->dataFinal > date("d/m/Y")){
-                        return true;
-                    }elseif($evento->dataFinal == date("d/m/Y")){
-                        if($evento->horarioFinal){
-                            if($evento->horarioFinal >= date("H:i")){
-                                return true;
-                            }
-                        }elseif($evento->horarioInicial >= date("H:i")){
-                            return true;
-                        }
-                    }
-                }elseif($evento->dataInicial > date("d/m/Y")){
-                    return true;
-                }elseif($evento->dataInicial == date("d/m/Y")){
-                    if($evento->horarioFinal){
-                        if($evento->horarioFinal >= date("H:i")){
-                            return true;
-                        }
-                    }elseif($evento->horarioInicial >= date("H:i")){
-                        return true;
-                    }
-                }
-                return false;
-            })->toJson(JSON_PRETTY_PRINT);
+        $eventos = Event::with(["idioma","modalidade","tipo","anexos"])->where("aprovado", true)->where(function($query){
+            $query->whereNotNull("dataFinal")->where("dataFinal",">=", date("Y-m-d"))
+                ->orWhere(function($query2){
+                    $query2->whereNull("dataFinal")->where("dataInicial", ">=", date("Y-m-d"));
+                });
+        })->get();
 
-        return response($eventos, 200);
+        return response($eventos->toJson(JSON_PRETTY_PRINT), 200);
     }
 
     public function getAllDefenses()
     {
         $defesas = Defense::with(["aluno.orientadores", "trabalho", "banca.membros"])
-            ->whereNotNull(["data","local","horario"])->get();
-        $defesas = $defesas->filter(function($defesa){
-            if($defesa->data > date("d/m/Y")){
-                return true;
-            }elseif($defesa->data == date("d/m/Y") and $defesa->horario >= date("H:i")){
-                return true;
-            }
-            return false;
-        })->toJson(JSON_PRETTY_PRINT);
-
-        return response($defesas,200);
+            ->whereNotNull(["data","local","horario"])->where("data", ">=", date("Y-m-d"))->get();
+            
+        return response($defesas->toJson(JSON_PRETTY_PRINT),200);
     }
 }
