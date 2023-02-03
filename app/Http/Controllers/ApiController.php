1d519a5eedd4ff66293f13ddf5ebe9cc2e5a816a
Ayrton Douglas Freitas Guimarães <ayrtondouglas@usp.br>
2023-02-03 17:30:23 -0300

    fix: saida json da api de defesas passa a ser em ordem ascendente #30

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Event;
 use App\Models\Defense;
+use Carbon\Carbon;
 
 class ApiController extends Controller
 {
@@ -24,7 +25,24 @@ class ApiController extends Controller
     {
         $defesas = Defense::with(["aluno.orientadores", "trabalho", "banca.membros.instituicao", "local"])->whereHas("local")
             ->whereNotNull(["data","horario"])->where("data", ">=", date("Y-m-d"))->get();
+        
+        $defesas = $defesas->sort(function($a,$b){
+            $dataDeA = Carbon::createFromFormat("d/m/Y", $a->data)->format("Y-m-d");
+            $dataDeB = Carbon::createFromFormat("d/m/Y", $b->data)->format("Y-m-d");
             
+            if($dataDeA > $dataDeB){
+                return 1;
+            }elseif($dataDeA < $dataDeB){
+                return -1;
+            }
+
+            if($a->horario > $b->horario){
+                return 1;
+            }elseif($a->horario < $b->horario){
+                return -1;
+            }
+        });
+
         return response($defesas->toJson(JSON_PRETTY_PRINT),200);
     }
 }
